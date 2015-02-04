aa8d54fb45c5bd681001090039419024665cf0b2
cflann <cflann2011@gmail.com>
2015-02-04 15:36:27 -0800

    finished linked list implementation, and analysis. we also covered an edge case for removing the tail  when it is also the head.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,35 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
+  //Constant time O(1)
   list.addToTail = function(value){
+    if (this.head === null) {
+      this.head = Node(value);
+      this.tail = this.head;
+    } else {
+      this.tail.next = Node(value);
+      this.tail = this.tail.next;
+    }
   };
-
+  //O(1)
   list.removeHead = function(){
+    var value = this.head;
+    delete this.head;
+    this.head = value.next;
+    this.tail = this.tail === value ? null : this.tail;
+    return value.value;
   };
-
+  //O(n)
   list.contains = function(target){
+    var current = this.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
