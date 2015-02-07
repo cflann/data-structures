a065d8eed79b5176c6b12732f8136fd8cad10653
cflann <cflann2011@gmail.com>
2015-02-06 20:56:56 -0800

    refactor set to work for all input types.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,23 +1,24 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = new HashTable();
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 //O(1)
 setPrototype.add = function(item){
+  var str = JSON.stringify(item);
   if (!this.contains(item)) {
-    this._storage.insert(item, item);
+    this._storage[str] = str;
   }
 };
 //O(1)
 setPrototype.contains = function(item){
-  return this._storage.retrieve(item) !== null;
+  return this._storage.hasOwnProperty(JSON.stringify(item));
 };
 //O(1)
 setPrototype.remove = function(item){
-  this._storage.remove(item);
+  delete this._storage[JSON.stringify(item)];
 };
 
 /*
